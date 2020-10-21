a021ab534c88cdf5676f7b680a7be770860e7eb8
huerepo <43037811+huerepo@users.noreply.github.com>
2020-10-21 18:48:24 -0500

    Update con.php

--- con.php
+++ con.php
@@ -1,1 +1,13 @@
 #DB CONNECTION
+<?php
+require_once '../../config.php';
+ 
+
+	$mysqli = new mysqli("localhost","emanvdos","emanvdospass","eman18v2"); 
+	
+	if(mysqli_connect_errno()){
+		echo 'Conexion Fallida : ', mysqli_connect_error();
+		exit();
+	}
+
+?>
